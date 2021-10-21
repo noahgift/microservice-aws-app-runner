a4c861517eb2291e385419d30be8446d628b2e71
EC2 Default User <ec2-user@ip-172-31-55-197.ec2.internal>
2021-10-21 18:16:51 +0000

    adding flask microservice

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+@app.route("/")
+def home():
+    html = f"<h3>Hi I am Flask</h3>"
+    return html.format(format)
+
+@app.route('/hi/<name>')
+def hello(name):
+    greeting = f"Hello: {name}"
+    return jsonify(greeting)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
